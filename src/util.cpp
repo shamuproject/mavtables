e0cf1ecf244c0d5ca269588c72c8af3e86a43aeb
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2017-12-22 12:36:30 -0700

    Change @ to \ style doxygen tags.

--- src/util.cpp
+++ src/util.cpp
@@ -20,15 +20,15 @@
 #include "util.hpp"
 
 
-/** @defgroup utility
+/** \defgroup utility
  *
  *  Utility functions that don't warrant their own file.
  */
 
 
-/** @brief Capitalize first letter of a string.
- *  @ingroup utility
- *  @bug Index error if given an empty string.
+/** \brief Capitalize first letter of a string.
+ *  \ingroup utility
+ *  \bug Index error if given an empty string.
  */
 std::string capital_case(std::string str)
 {
