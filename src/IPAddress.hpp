7c2d8b6f0662580b3ca0de669a387103d5103f0f
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2017-12-29 16:48:34 -0700

    Add exceptions for out of range arguments.

--- src/IPAddress.hpp
+++ src/IPAddress.hpp
@@ -32,6 +32,7 @@ class IPAddress
     private:
         unsigned long address_;
         unsigned int port_;
+        void construct_(unsigned long address, unsigned int port);
 
     public:
         /** Copy constructor.
@@ -40,8 +41,8 @@ class IPAddress
          */
         IPAddress(const IPAddress &other) = default;
         IPAddress(const IPAddress &other, unsigned int port);
-        IPAddress(std::string address);
         IPAddress(unsigned long address, unsigned int port = 0);
+        IPAddress(std::string address);
         unsigned long address() const;
         unsigned int port() const;
 
