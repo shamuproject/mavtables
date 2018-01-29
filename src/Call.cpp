3a614d41b225b1038c08f016eac19e49a6484635
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-01-28 21:18:32 -0700

    Add Call class.

---
+++ src/Call.cpp
@@ -0,0 +1,53 @@
+// MAVLink router and firewall.
+// Copyright (C) 2018  Michael R. Shannon <mrshannon.aerospace@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#include <ostream>
+
+#include "Packet.hpp"
+#include "MAVAddress.hpp"
+#include "Action.hpp"
+#include "Chain.hpp"
+#include "Call.hpp"
+
+
+Call::Call(std::shared_ptr<Chain> chain)
+    : chain_(std::move(chain))
+{
+}
+
+
+/** \copydoc Action::print_(std::ostream &os) const
+ *
+ *  Prints `"call <Chain Name>"`.
+ */
+std::ostream &Call::print_(std::ostream &os) const
+{
+    os << "call " << chain_->name;
+    return os;
+}
+
+
+/** \copydoc Action::action(const Packet&,const MAVAddress&,RecursionChecker&)const
+ *
+ *  The Call class delegates the action choice to the contained \ref Chain.
+ */
+Action::Option Call::action(
+    const Packet &packet, const MAVAddress &address,
+    RecursionChecker &recursion_checker) const
+{
+    return chain_->action(packet, address, recursion_checker);
+}
---
+++ test/test_Call.cpp
@@ -0,0 +1,113 @@
+// MAVLink router and firewall.
+// Copyright (C) 2018  Michael R. Shannon <mrshannon.aerospace@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#include <memory>
+
+#include <catch.hpp>
+
+#include "util.hpp"
+#include "Packet.hpp"
+#include "PacketVersion1.hpp"
+#include "PacketVersion2.hpp"
+#include "MAVAddress.hpp"
+#include "MAVSubnet.hpp"
+#include "RecursionChecker.hpp"
+#include "Chain.hpp"
+#include "Action.hpp"
+#include "Call.hpp"
+
+#include "common_Packet.hpp"
+
+
+namespace
+{
+
+    class ChainTestClass : public Chain
+    {
+        public:
+            using Chain::Chain;
+            virtual Action::Option action(
+                const Packet &packet, const MAVAddress &address,
+                RecursionChecker &recursion_checker) const
+            {
+                (void)recursion_checker;
+
+                if (packet.name() == "PING")
+                {
+                    if (MAVSubnet("192.0/14").contains(address))
+                    {
+                        return Action::ACCEPT;
+                    }
+
+                    return Action::REJECT;
+                }
+
+                return Action::CONTINUE;
+            }
+    };
+
+}
+
+
+TEST_CASE("Call's can be constructed.", "[Call]")
+{
+    REQUIRE_NOTHROW(Call(std::make_shared<ChainTestClass>("test_chain")));
+}
+
+
+TEST_CASE("Call's 'action' method always returns Action::ACCEPT.",
+          "[Call]")
+{
+    auto conn = std::make_shared<ConnectionTestClass>();
+    auto ping = packet_v2::Packet(to_vector(PingV2()), conn);
+    auto hb = packet_v1::Packet(to_vector(HeartbeatV1()), conn);
+    RecursionChecker rc;
+    Call call(std::make_shared<ChainTestClass>("test_chain"));
+    REQUIRE(call.action(ping, MAVAddress("192.0"), rc) == Action::ACCEPT);
+    REQUIRE(call.action(ping, MAVAddress("192.1"), rc) == Action::ACCEPT);
+    REQUIRE(call.action(ping, MAVAddress("192.2"), rc) == Action::ACCEPT);
+    REQUIRE(call.action(ping, MAVAddress("192.3"), rc) == Action::ACCEPT);
+    REQUIRE(call.action(ping, MAVAddress("192.4"), rc) == Action::REJECT);
+    REQUIRE(call.action(ping, MAVAddress("192.5"), rc) == Action::REJECT);
+    REQUIRE(call.action(ping, MAVAddress("192.6"), rc) == Action::REJECT);
+    REQUIRE(call.action(ping, MAVAddress("192.7"), rc) == Action::REJECT);
+    REQUIRE(call.action(hb, MAVAddress("192.0"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.1"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.2"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.3"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.4"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.5"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.6"), rc) == Action::CONTINUE);
+    REQUIRE(call.action(hb, MAVAddress("192.7"), rc) == Action::CONTINUE);
+}
+
+
+TEST_CASE("Call's are printable.", "[Call]")
+{
+    auto conn = std::make_shared<ConnectionTestClass>();
+    auto ping = packet_v2::Packet(to_vector(PingV2()), conn);
+    Call call(std::make_shared<ChainTestClass>("test_chain"));
+    Action &action = call;
+    SECTION("By direct type.")
+    {
+        REQUIRE(str(call) == "call test_chain");
+    }
+    SECTION("By polymorphic type.")
+    {
+        REQUIRE(str(action) == "call test_chain");
+    }
+}
