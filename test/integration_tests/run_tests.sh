99eb16283fb92c1439a1173209f4305cbc45cf05
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-04-06 18:24:08 -0600

    Disable integration testing for Mac OS X.
    The integration testing script does not currently work on Mac OS X and
    it's development is on permanent hold until a Mac OS X developer joins
    the project.  Unit testing is still supported on Mac OS X.

--- test/integration_tests/run_tests.sh
+++ test/integration_tests/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+FAIL=0
+
+
 function dir() {
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     echo "$DIR"
@@ -11,7 +14,6 @@ source "$(dir)/../ansi_codes.sh"
 
 
 function run_test() {
-    ps
     PAD=$(printf '%0.1s' "."{1..80})
     printf "${_BOLD}%s" "$1  "
     $2
@@ -20,17 +22,16 @@ function run_test() {
         printf "%*.*s" 0 $((67 - ${#1})) "$PAD"
         printf "  ${_RED}%s${ANSI_RESET}\n" "[FAILED]"
         echo "$DIFF" | tail -n +2
-        return 1
+        FAIL=1
     else
         printf "%*.*s" 0 $((66 - ${#1})) "$PAD"
         printf "  ${_GREEN}%s${ANSI_RESET}\n" "[SUCCESS]"
-        return 0
     fi
 }
 
 
-function shutdown_pids() {
-    array=("$@")
+function shutdown_background() {
+    array=($(jobs -p))
     for ((i = ${#array[@]} - 1; i >= 0; i--)); do
         kill -SIGINT ${array[i]} >/dev/null
         sleep 0.5
@@ -52,107 +53,102 @@ function test_complex_ast_printing() {
 
 
 function test_all_v1_packets_udp() {
-    PIDS=()
     socat pty,link=./ttyS0,raw,echo=0 pty,link=./ttyS1,raw,echo=0 &
-    PIDS+=($!)
     "$(dir)/../../build/mavtables" --conf "$(dir)/all_udp.conf" &
-    PIDS+=($!)
     "$(dir)/logger.py" 20 10 --udp 127.0.0.1:14500 \
         > "$(dir)/all_v1_packets_udp_to_udp.log" &
-    PIDS+=($!)
     "$(dir)/logger.py" 10 20 --serial ./ttyS1 \
         > "$(dir)/all_v1_packets_udp_to_serial.log" &
-    PIDS+=($!)
     sleep 0.5
     "$(dir)/packet_scripter.py" 3 10 "$(dir)/all_v1_packets.pks" \
         --udp 127.0.0.1:14500 --mavlink1
     sleep 0.5
-    shutdown_pids "${PIDS[@]}"
+    shutdown_background
 }
 
 
 function test_all_v2_packets_udp() {
-    PIDS=()
     socat pty,link=./ttyS0,raw,echo=0 pty,link=./ttyS1,raw,echo=0 &
-    PIDS+=($!)
     "$(dir)/../../build/mavtables" --conf "$(dir)/all_udp.conf" &
-    PIDS+=($!)
     "$(dir)/logger.py" 20 10 --udp 127.0.0.1:14500 \
         > "$(dir)/all_v2_packets_udp_to_udp.log" &
-    PIDS+=($!)
     "$(dir)/logger.py" 10 20 --serial ./ttyS1 \
         > "$(dir)/all_v2_packets_udp_to_serial.log" &
-    PIDS+=($!)
     sleep 0.5
     "$(dir)/packet_scripter.py" 3 10 "$(dir)/all_v2_packets.pks" \
         --udp 127.0.0.1:14500
     sleep 0.5
-    shutdown_pids "${PIDS[@]}"
+    shutdown_background
 }
 
 
 function test_all_v1_packets_serial() {
-    PIDS=()
     socat pty,link=./ttyS0,raw,echo=0 pty,link=./ttyS1,raw,echo=0 &
-    PIDS+=($!)
     socat pty,link=./ttyS2,raw,echo=0 pty,link=./ttyS3,raw,echo=0 &
-    PIDS+=($!)
     "$(dir)/../../build/mavtables" --conf "$(dir)/all_serial.conf" &
-    PIDS+=($!)
     "$(dir)/logger.py" 20 10 --udp 127.0.0.1:14500 \
         > "$(dir)/all_v1_packets_serial_to_udp.log" &
-    PIDS+=($!)
     "$(dir)/logger.py" 10 20 --serial ./ttyS1 \
         > "$(dir)/all_v1_packets_serial_to_serial.log" &
-    PIDS+=($!)
     sleep 0.5
     "$(dir)/packet_scripter.py" 3 10 "$(dir)/all_v1_packets.pks" \
         --serial ./ttyS3 --mavlink1
     sleep 0.5
-    shutdown_pids "${PIDS[@]}"
+    shutdown_background
 }
 
 
 function test_all_v2_packets_serial() {
-    PIDS=()
     socat pty,link=./ttyS0,raw,echo=0 pty,link=./ttyS1,raw,echo=0 &
-    PIDS+=($!)
     socat pty,link=./ttyS2,raw,echo=0 pty,link=./ttyS3,raw,echo=0 &
-    PIDS+=($!)
     "$(dir)/../../build/mavtables" --conf "$(dir)/all_serial.conf" &
-    PIDS+=($!)
     "$(dir)/logger.py" 20 10 --udp 127.0.0.1:14500 \
         > "$(dir)/all_v2_packets_serial_to_udp.log" &
-    PIDS+=($!)
     "$(dir)/logger.py" 10 20 --serial ./ttyS1 \
         > "$(dir)/all_v2_packets_serial_to_serial.log" &
-    PIDS+=($!)
     sleep 0.5
     "$(dir)/packet_scripter.py" 3 10 "$(dir)/all_v2_packets.pks" \
         --serial ./ttyS3
     sleep 0.5
-    shutdown_pids "${PIDS[@]}"
+    shutdown_background
 }
 
 
-echo -en "${_BOLD}${_BLUE}*---------------------------------------"
+echo -en "${_BOLD}${_MAGENTA}*---------------------------------------"
 echo -en "--------------------------------------*\n"
-echo -en "${_BOLD}${_BLUE}|                              "
+echo -en "${_BOLD}${_MAGENTA}|                              "
 echo -en "Integration Tests"
 echo -en "                              |\n"
-echo -en "${_BOLD}${_BLUE}*---------------------------------------"
+echo -en "${_BOLD}${_MAGENTA}*---------------------------------------"
 echo -en "--------------------------------------*\n"
 echo -en "${ANSI_RESET}"
 
 
 rm "$(dir)"/*.log 2>/dev/null
-FAIL=0
+
+
+case "$OSTYPE" in
+  solaris*)
+      ;;
+  darwin*)
+      echo -en "${_BOLD}${_ITALICS}${_RED}Integration testing is not currently "
+      echo -en "supported on Mac OS X.${ANSI_RESET}\n\n"
+      exit 0
+      ;;
+  linux*)
+      ;;
+  bsd*)
+      ;;
+  *)
+      echo "unknown operating system: $OSTYPE"
+      exit 1
+      ;;
+esac
 
 
 function check_fail() {
     if [ "$?" -ne "0" ]; then
         FAIL=1
-        # exit $FAIL;
     fi
 }
 
@@ -161,56 +157,47 @@ run_test "Abstract Syntax Tree printing with --ast flag" \
     test_ast_printing \
     mavtables.cmp \
     mavtables.log
-check_fail
 run_test "Complex Abstract Syntax Tree printing with --ast flag" \
     test_complex_ast_printing \
     complex_config.cmp \
     complex_config.log
-check_fail
 run_test "MAVLink v1.0 packets from UDP to UDP" \
     test_all_v1_packets_udp \
     all_v1_packets.pks \
     all_v1_packets_udp_to_udp.log
-check_fail
 run_test "MAVLink v1.0 packets from UDP to serial port" \
     test_all_v1_packets_udp \
     all_v1_packets.pks \
     all_v1_packets_udp_to_serial.log
-check_fail
 run_test "MAVLink v2.0 packets from UDP to UDP" \
     test_all_v2_packets_udp \
     all_v2_packets.pks \
     all_v2_packets_udp_to_udp.log
-check_fail
 run_test "MAVLink v2.0 packets from UDP to serial port" \
     test_all_v2_packets_udp \
     all_v2_packets.pks \
     all_v2_packets_udp_to_serial.log
-check_fail
 
 
 run_test "MAVLink v1.0 packets from serial port to UDP" \
     test_all_v1_packets_serial \
     all_v1_packets.pks \
     all_v1_packets_serial_to_udp.log
-check_fail
 run_test "MAVLink v1.0 packets from serial port to serial port" \
     test_all_v1_packets_serial \
     all_v1_packets.pks \
     all_v1_packets_serial_to_serial.log
-check_fail
 run_test "MAVLink v2.0 packets from serial port to UDP" \
     test_all_v2_packets_serial \
     all_v2_packets.pks \
     all_v2_packets_serial_to_udp.log
-check_fail
 run_test "MAVLink v2.0 packets from serial port to serial port" \
     test_all_v2_packets_serial \
     all_v2_packets.pks \
     all_v2_packets_serial_to_serial.log
-check_fail
 
 
 if [ "$FAIL" -ne "0" ]; then
-    exit $FAIL;
+    echo -en "\n"
+    exit $FAIL
 fi
